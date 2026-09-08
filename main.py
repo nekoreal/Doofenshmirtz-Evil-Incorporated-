a41626bdc154360604e456b6a7b4599eaadf4b04
NekoReal <amakoev8938@gmail.com>
2026-09-09 01:15:32 +0300

    feat: ad health check route

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 
+ 
 
+app = FastAPI(title="Doofenshmirtz Evil Inc API")
 
-app = FastAPI()
-
-@app.get("/home", summary="Get home page", tags=["Home"])
-def home():
-    return {"message": "Hello, World!"}
-
+@app.get("/health", tags=["Health"])
+def health_check():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     #uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000) 
